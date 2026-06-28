e7632131aac1de7eccc65e95e1f01925beb01716
dev-rome <info.devrome@gmail.com>
2026-06-28 01:43:56 -0400

    create table sessions

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,10 @@ CREATE TABLE tasks (
   created_at TIMESTAMPTZ NOT NULL DEFAULT now(),  
   updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 )
+
+CREATE TABLE sessions(
+  id TEXT PRIMARY KEY,
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  expires_at TIMESTAMPTZ NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+)
